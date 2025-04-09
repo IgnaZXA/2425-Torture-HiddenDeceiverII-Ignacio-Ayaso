282f8b06206b6ec95b6805db5319799f7f27b6a4
Ignacio <ignayh2000@gmail.com>
2025-04-09 12:41:24 +0200

    [ADD] Attack method

--- character.mjs
+++ character.mjs
@@ -8,4 +8,23 @@ export default class Character{
         this.weapon = weapon;           // Arma equipada. Contrendrá el objeto de la clase Weapon
         this.pouch = pouch;             // Bolsa del equipaje. Contendrá un array de objetos P
     }
+
+    // Simular un ataque de un personaje a otro
+    attack(enemy){
+
+        let weaponDamage = 0;
+        
+        for(let i = 0; i < this.weapon.numDieDamage; i++){
+            let d6 = Math.floor(Math.random() * 6) + 1; [0, 5] + 1 --> [1, 6]
+            weaponDamage += d6;
+        }
+
+
+        enemy.life -= weaponDamage;
+        this.weapon.quality -= 3;
+
+        return weaponDamage;
+    }
+
+
 }
